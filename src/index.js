6b8cb2dab7830ea0f63418af9b9310eaf4536a53
Eugene <egor_egorov_qwerty@mail.ru>
2021-07-14 23:20:10 +0300

    complete move

--- src/index.js
+++ src/index.js
@@ -25,86 +25,86 @@ let leftPress = false
 let pY = 270
 let pX = 270
 
-function keyDownHandler(e) {
-    if (e.key === 'Up' || e.key === 'ArrowUp') {
-        upPress = true
-    }
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-        rightPress = true
-    }
-    if (e.key === 'Down' || e.key === 'ArrowDown') {
-        bottomPress = true
-    }
-    if (e.key === 'Left' || e.key === 'ArrowLeft') {
-        leftPress = true
-    }
-}
+// function keyDownHandler(e) {
+//     if (e.key === 'Up' || e.key === 'ArrowUp') {
+//         upPress = true
+//     }
+//     if (e.key === 'Right' || e.key === 'ArrowRight') {
+//         rightPress = true
+//     }
+//     if (e.key === 'Down' || e.key === 'ArrowDown') {
+//         bottomPress = true
+//     }
+//     if (e.key === 'Left' || e.key === 'ArrowLeft') {
+//         leftPress = true
+//     }
+// }
 
-function keyUpHandler(e) {
-    if (e.key === 'Up' || e.key === 'ArrowUp') {
-        upPress = false
-    }
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-        rightPress = false
-    }
-    if (e.key === 'Down' || e.key === 'ArrowDown') {
-        bottomPress = false
-    }
-    if (e.key === 'Left' || e.key === 'ArrowLeft') {
-        leftPress = false
-    }
-}
+// function keyUpHandler(e) {
+//     if (e.key === 'Up' || e.key === 'ArrowUp') {
+//         upPress = false
+//     }
+//     if (e.key === 'Right' || e.key === 'ArrowRight') {
+//         rightPress = false
+//     }
+//     if (e.key === 'Down' || e.key === 'ArrowDown') {
+//         bottomPress = false
+//     }
+//     if (e.key === 'Left' || e.key === 'ArrowLeft') {
+//         leftPress = false
+//     }
+// }
 
-document.addEventListener('keydown', keyDownHandler)
-document.addEventListener('keyup', keyUpHandler)
+// document.addEventListener('keydown', keyDownHandler)
+// document.addEventListener('keyup', keyUpHandler)
 
-const img = document.createElement('img')
-img.src = SenseiWalk
-
-function walk() {
-    if (upPress) {
-        cycle = (cycle + 1) % shots
-        direction = 144
-        if (pY !== 0) {
-            pY += -10
-        }
-    }
-    if (rightPress) {
-        cycle = (cycle + 1) % shots
-        direction = 96
-        if (pX !== 560) {
-            pX += 10
-        }
-    }
-    if (bottomPress) {
-        cycle = (cycle + 1) % shots
-        direction = 0
-        if (pY !== 550) {
-            pY += 10
-        }
-    }
-    if (leftPress) {
-        cycle = (cycle + 1) % shots
-        direction = 48
-        if (pX !== 0) {
-            pX += -10
-        }
-    }
-
-    ctx.clearRect(0, 0, 600, 600)
-    ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48)
-
-    window.requestAnimationFrame(walk)
-}
-
-img.addEventListener('load', () => {
-    loading.style.display = 'none'
-
-    window.requestAnimationFrame(walk)
-})
-
-const terrain = document.createElement('img')
-terrain.src = terrainAtlas
+// const img = document.createElement('img')
+// img.src = SenseiWalk
+//
+// function walk() {
+//     if (upPress) {
+//         cycle = (cycle + 1) % shots
+//         direction = 144
+//         if (pY !== 0) {
+//             pY += -10
+//         }
+//     }
+//     if (rightPress) {
+//         cycle = (cycle + 1) % shots
+//         direction = 96
+//         if (pX !== 560) {
+//             pX += 10
+//         }
+//     }
+//     if (bottomPress) {
+//         cycle = (cycle + 1) % shots
+//         direction = 0
+//         if (pY !== 550) {
+//             pY += 10
+//         }
+//     }
+//     if (leftPress) {
+//         cycle = (cycle + 1) % shots
+//         direction = 48
+//         if (pX !== 0) {
+//             pX += -10
+//         }
+//     }
+//
+//     ctx.clearRect(0, 0, 600, 600)
+//     ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48)
+//
+//     window.requestAnimationFrame(walk)
+// }
+//
+// img.addEventListener('load', () => {
+//     loading.style.display = 'none'
+//
+//     window.requestAnimationFrame(walk)
+// })
+//
+// const terrain = document.createElement('img')
+// terrain.src = terrainAtlas
 
 // terrain.addEventListener('load', () => {
 //     const {map} = worldCfg
